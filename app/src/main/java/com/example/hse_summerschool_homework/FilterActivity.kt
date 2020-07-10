793e6e252b7775eb6fb7a5575130e3971a54a4ee
exsandebest <exsandebest@yandex.ru>
2020-07-10 20:56:31 +0300

    Apply button returns values

--- app/src/main/java/com/example/hse_summerschool_homework/FilterActivity.kt
+++ app/src/main/java/com/example/hse_summerschool_homework/FilterActivity.kt
@@ -1,11 +1,18 @@
 package com.example.hse_summerschool_homework
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.children
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_filter.*
+import kotlinx.android.synthetic.main.filter_item.view.*
 
 class FilterActivity : AppCompatActivity() {
+
+    private val boxes = booleanArrayOf(false, false, false, false, false)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_filter)
@@ -19,5 +26,15 @@ class FilterActivity : AppCompatActivity() {
                 FilterItem("> 5 лет", 5.5)
             )
         )
+
+        applyButton.setOnClickListener {
+            var idx = 0
+            for (v in recycler.children){
+                boxes[idx] = v.checkBox.isChecked
+                ++idx
+            }
+            setResult(Activity.RESULT_OK, Intent().putExtra("filter", boxes))
+            finish()
+        }
     }
 }
